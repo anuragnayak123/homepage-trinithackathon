ba20e2bfd5021794b61aade116e3a893e446f687
K-S-Abhihkeshav <97213738+K-S-Abhihkeshav@users.noreply.github.com>
2023-02-11 00:18:32 +0530

    Update homepage.js

--- homepage.js
+++ homepage.js
@@ -1,6 +1,19 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
-
+function home(){
+    return(
+        <body>
+            <nav>
+                home
+                <a href=""><button>login</button></a>
+                <a href=""><button>signup</button></a>
+            </nav>
+            <h1>title</h1>
+            <iframe src="https://giphy.com/embed/26gs9jTY1R02ueEnu" width="480" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/engine-farm-agriculture-26gs9jTY1R02ueEnu">via GIPHY</a></p>
+        </body>
+    );
+}
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render( < h1 > Hello World < /h1>);
+root.render(home);
+export default home;
